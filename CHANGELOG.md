9d6ab413ad6c4e48ff9166b66fb639c922c0e509
deissh <deissh@yandex.ru>
2020-10-06 11:21:01 +0300

    feat: send message to telegram (#1)
    * feat: send message to tg
    
    * fix: debug log
    
    * feat: update command file
    
    * fix(commands/): bool type -> boolean
    
    * feat: add jobs and example
    
    * fix: example/send_msg.yml
    
    * fix: job.yml
    
    * fix: code style
    
    * fix: code style
    
    * fix: code style
    
    * fix: code style
    
    * fix: integration tests
    
    * feat: .circleci send telegram msg
    
    * fix: tabs and exec curl
    
    * fix: disable SC2086
    
    * fix: env vars
    
    * fix: env warnings
    
    * ci: trigger
    
    * [semver:patch]: chore
    
    * fix: ci

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,11 +9,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [1.0.0] - YYYY-MM-DD
 ### Added
- - Initial Release
+
 ### Changed
- - Initial Release
+
 ### Removed
- - Initial Release
+
 
 
 [1.0.0]: GITHUB TAG URL
---
+++ src/scripts/send_msg.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#title          :send_msg.sh
+#author         :deissh
+#version        :0.1
+#===============================
+readonly API_URL=${TELEGRAM_API_URL:-"https://api.telegram.org/bot"}
+readonly ORB_TEST_ENV="bats-core"
+
+# default values
+DEBUG=false
+TOKEN=""
+CHATS=()
+CURL_OPTIONS="-s"
+DISABLE_NOTIFICATION=false
+
+
+function log {
+    [ "$DEBUG" = true ] && echo "DEBUG: $1"
+}
+
+[ -z "$TOKEN" ] && TOKEN=$TELEGRAM_TOKEN
+[ ${#CHATS[@]} -eq 0 ] && CHATS=( "$TELEGRAM_CHAT" )
+[ -n "$TELEGRAM_DISABLE_NOTIFICATION" ] && DISABLE_NOTIFICATION="$TELEGRAM_DISABLE_NOTIFICATION"
+
+if [ -z "$TOKEN" ]; then
+    echo "No bot token was given."
+    exit 1
+fi
+
+if [ ${#CHATS[@]} -eq 0 ]; then
+    echo "No chats given."
+    exit 1
+fi
+
+log "TOKEN=$TOKEN"
+log "CHATS=${CHATS[*]}"
+log "DISABLE_WEB_PAGE_PREVIEW=$DISABLE_WEB_PAGE_PREVIEW"
+log "DISABLE_NOTIFICATION=$DISABLE_NOTIFICATION"
+
+
+shift $((OPTIND - 1))
+TEXT="$1"
+[ "$TEXT" = "--" ] && TEXT=$(</dev/stdin)
+[ -z "$TEXT" ] && [ -n "$TELEGRAM_TEXT" ] && TEXT="$TELEGRAM_TEXT"
+
+log "Text: $TEXT"
+
+CURL_OPTIONS="$CURL_OPTIONS --form text=<-"
+[ "$DISABLE_NOTIFICATION" = true ] && CURL_OPTIONS="$CURL_OPTIONS --form-string disable_notification=true"
+
+for CHAT_ID in "${CHATS[@]}"; do
+    MY_CURL_OPTIONS="$CURL_OPTIONS --form-string chat_id=$CHAT_ID"
+
+    # Entrypoint
+    # Will not run if sourced for bats-core tests.
+    if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+        # shellcheck disable=SC2086
+        response=$(curl $MY_CURL_OPTIONS "$API_URL$TOKEN/sendMessage" <<< "$TEXT")
+        status=$?
+    else
+        echo "Executing: curl $MY_CURL_OPTIONS"
+        status=0
+        response='{"ok": true}'
+    fi
+
+    echo "Response was: $response"
+    if [ $status -ne 0 ]; then
+        echo "curl reported an error. Exit code was: $status."
+        echo "Response was: $response"
+        echo "Quitting."
+        exit $status
+    fi
+
+    if [[ "$response" != '{"ok":true'* ]]; then
+        echo "Telegram reported an error:"
+        echo "$response"
+        echo "Quitting."
+        exit 1
+    fi
+done
