392df99c9f32afb150d2c26520980cb04cdc5dac
deissh <deissh@yandex.ru>
2020-10-07 09:02:57 +0300

    [semver:major] send_msg (#2)
    * chore: version bump
    
    * fix ci
    
    * fix ci
    
    * fix ci
    
    * fix: script header
    
    * fix: bash script
    
    * fix: remove useless var
    
    * chore: debug log
    
    * fix: curl options
    
    * feat: add log
    
    * fix: command file
    
    * refactor: commands
    
    * fix: command file
    
    * fix: command file
    
    * fix: command file
    
    * fix: yaml format
    
    * fix: yaml format

--- src/scripts/send_msg.sh
+++ src/scripts/send_msg.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 #title          :send_msg.sh
 #author         :deissh
 #version        :0.1
@@ -7,19 +6,15 @@ readonly API_URL=${TELEGRAM_API_URL:-"https://api.telegram.org/bot"}
 readonly ORB_TEST_ENV="bats-core"
 
 # default values
-DEBUG=false
 TOKEN=""
 CHATS=()
 CURL_OPTIONS="-s"
 DISABLE_NOTIFICATION=false
 
 
-function log {
-    [ "$DEBUG" = true ] && echo "DEBUG: $1"
-}
-
 [ -z "$TOKEN" ] && TOKEN=$TELEGRAM_TOKEN
-[ ${#CHATS[@]} -eq 0 ] && CHATS=( "$TELEGRAM_CHAT" )
+# shellcheck disable=SC2206
+[ ${#CHATS[@]} -eq 0 ] && CHATS=( $TELEGRAM_CHAT )
 [ -n "$TELEGRAM_DISABLE_NOTIFICATION" ] && DISABLE_NOTIFICATION="$TELEGRAM_DISABLE_NOTIFICATION"
 
 if [ -z "$TOKEN" ]; then
@@ -32,18 +27,11 @@ if [ ${#CHATS[@]} -eq 0 ]; then
     exit 1
 fi
 
-log "TOKEN=$TOKEN"
-log "CHATS=${CHATS[*]}"
-log "DISABLE_WEB_PAGE_PREVIEW=$DISABLE_WEB_PAGE_PREVIEW"
-log "DISABLE_NOTIFICATION=$DISABLE_NOTIFICATION"
-
-
-shift $((OPTIND - 1))
-TEXT="$1"
-[ "$TEXT" = "--" ] && TEXT=$(</dev/stdin)
-[ -z "$TEXT" ] && [ -n "$TELEGRAM_TEXT" ] && TEXT="$TELEGRAM_TEXT"
-
-log "Text: $TEXT"
+[ -n "$TELEGRAM_TEXT" ] && TEXT="$TELEGRAM_TEXT"
+if [ ${#CHATS[@]} -eq 0 ]; then
+    echo "No text given."
+    exit 1
+fi
 
 CURL_OPTIONS="$CURL_OPTIONS --form text=<-"
 [ "$DISABLE_NOTIFICATION" = true ] && CURL_OPTIONS="$CURL_OPTIONS --form-string disable_notification=true"
@@ -51,6 +39,9 @@ CURL_OPTIONS="$CURL_OPTIONS --form text=<-"
 for CHAT_ID in "${CHATS[@]}"; do
     MY_CURL_OPTIONS="$CURL_OPTIONS --form-string chat_id=$CHAT_ID"
 
+    echo "Executing: curl $MY_CURL_OPTIONS"
+    echo "$API_URL$TOKEN/sendMessage"
+    
     # Entrypoint
     # Will not run if sourced for bats-core tests.
     if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
@@ -58,12 +49,10 @@ for CHAT_ID in "${CHATS[@]}"; do
         response=$(curl $MY_CURL_OPTIONS "$API_URL$TOKEN/sendMessage" <<< "$TEXT")
         status=$?
     else
-        echo "Executing: curl $MY_CURL_OPTIONS"
         status=0
         response='{"ok": true}'
     fi
 
-    echo "Response was: $response"
     if [ $status -ne 0 ]; then
         echo "curl reported an error. Exit code was: $status."
         echo "Response was: $response"
