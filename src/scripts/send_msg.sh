815fcdf0b328a1b70cffcef588671cae07695c3b
deissh <deissh@yandex.ru>
2020-10-07 08:00:39 +0300

    fix: bash script

--- src/scripts/send_msg.sh
+++ src/scripts/send_msg.sh
@@ -13,10 +13,6 @@ CURL_OPTIONS="-s"
 DISABLE_NOTIFICATION=false
 
 
-function log {
-    [ "$DEBUG" = true ] && echo "DEBUG: $1"
-}
-
 [ -z "$TOKEN" ] && TOKEN=$TELEGRAM_TOKEN
 [ ${#CHATS[@]} -eq 0 ] && CHATS=( "$TELEGRAM_CHAT" )
 [ -n "$TELEGRAM_DISABLE_NOTIFICATION" ] && DISABLE_NOTIFICATION="$TELEGRAM_DISABLE_NOTIFICATION"
@@ -31,18 +27,10 @@ if [ ${#CHATS[@]} -eq 0 ]; then
     exit 1
 fi
 
-log "TOKEN=$TOKEN"
-log "CHATS=${CHATS[*]}"
-log "DISABLE_WEB_PAGE_PREVIEW=$DISABLE_WEB_PAGE_PREVIEW"
-log "DISABLE_NOTIFICATION=$DISABLE_NOTIFICATION"
-
 
 shift $((OPTIND - 1))
 TEXT="$1"
-[ "$TEXT" = "--" ] && TEXT=$(</dev/stdin)
 [ -z "$TEXT" ] && [ -n "$TELEGRAM_TEXT" ] && TEXT="$TELEGRAM_TEXT"
-
-log "Text: $TEXT"
 
 CURL_OPTIONS="$CURL_OPTIONS --form text=<-"
 [ "$DISABLE_NOTIFICATION" = true ] && CURL_OPTIONS="$CURL_OPTIONS --form-string disable_notification=true"
